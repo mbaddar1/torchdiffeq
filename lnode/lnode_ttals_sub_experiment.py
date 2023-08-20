203a7bf8f69b71fd9838ed64f8043b8ae65db155
mbaddar <mbaddar2@gmail.com>
2023-08-20 15:40:49 +0300

    continue checking the hybrid trajectory generation approach

--- lnode/lnode_ttals_sub_experiment.py
+++ lnode/lnode_ttals_sub_experiment.py
@@ -32,9 +32,6 @@ from examples.models import CNF
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 
-# Global-vars
-N_SAMPLES = 4096
-
 parser = argparse.ArgumentParser()
 parser.add_argument('--artifact', type=str, required=True)
 parser.add_argument('--adjoint', action='store_true')
@@ -45,6 +42,7 @@ parser.add_argument('--rank', type=int, required=True)
 parser.add_argument('--degree', type=int, required=True)
 parser.add_argument('--h-steps', type=int, required=True)
 parser.add_argument('--tol', type=float, required=True)
+parser.add_argument('--n-samples', type=int, required=True)
 
 # parser.add_argument('--t0-index', type=int, required=True)
 args = parser.parse_args()
@@ -79,7 +77,7 @@ def adjust_tensor_to_domain(x: torch.Tensor, domain_stripe: List[float]):
 
 
 def run_tt_als(x: torch.Tensor, t: float, y: torch.Tensor, poly_degree: int, rank: int, test_ratio: float,
-               tol: float) -> Extended_TensorTrain:
+               tol: float) -> List[Extended_TensorTrain]:
     N = x.shape[0]
     N_test = int(test_ratio * N)
     N_train = N - N_test
@@ -94,34 +92,35 @@ def run_tt_als(x: torch.Tensor, t: float, y: torch.Tensor, poly_degree: int, ran
     domain = [[-1., 1.] for _ in range(Dx)]
     domain_stripe = domain[0]
     op = orthpoly(degrees, domain)
-
-    ETT = Extended_TensorTrain(op, ranks)
-    for d_y in range(Dy):
-        y_d = y[:, d_y].view(-1, 1)
+    x_domain_adjusted = adjust_tensor_to_domain(x=x, domain_stripe=domain_stripe)
+    ETTs = [Extended_TensorTrain(op, ranks) for _ in range(Dy)]
+    for j in range(Dy):
+        y_d = y[:, j].view(-1, 1)
         # ALS parameters
         reg_coeff = 1e-2
         iterations = 40
-        x_domain_adjusted = adjust_tensor_to_domain(x=x, domain_stripe=domain_stripe)
         rule = None
         # rule = tt.Dörfler_Adaptivity(delta = 1e-6,  maxranks = [32]*(n-1), dims = [feature_dim]*n, rankincr = 1)
-        ETT.fit(x=x_domain_adjusted.type(torch.float64)[:N_train, :], y=y_d.type(torch.float64)[:N_train, :],
-                iterations=iterations, rule=rule, tol=tol,
-                verboselevel=1, reg_param=reg_coeff)
-        ETT.tt.set_core(Dx - 1)
-        train_error = (torch.norm(ETT(x_domain_adjusted.type(torch.float64)[:N_train, :]) -
+        ETTs[j].fit(x=x_domain_adjusted.type(torch.float64)[:N_train, :], y=y_d.type(torch.float64)[:N_train, :],
+                    iterations=iterations, rule=rule, tol=tol,
+                    verboselevel=1, reg_param=reg_coeff)
+        ETTs[j].tt.set_core(Dx - 1)
+        train_error = (torch.norm(ETTs[j](x_domain_adjusted.type(torch.float64)[:N_train, :]) -
                                   y_d.type(torch.float64)[:N_train, :]) ** 2 / torch.norm(
             y_d.type(torch.float64)[:N_train, :]) ** 2).item()
-        val_error = (torch.norm(ETT(x_domain_adjusted.type(torch.float64)[N_train:, :]) -
+        val_error = (torch.norm(ETTs[j](x_domain_adjusted.type(torch.float64)[N_train:, :]) -
                                 y_d.type(torch.float64)[N_train:, :]) ** 2 / torch.norm(
             y_d.type(torch.float64)[N_train:, :]) ** 2).item()
-        logger.info(f'For d_y  = {d_y} :TT-ALS Relative error on training set = {train_error}')
-        logger.info(f'For d_y = {d_y}: TT-ALS Relative error on test set = {val_error}')
+        logger.info(f'For j ( d-th dim of y)  = {j} :TT-ALS Relative error on training set = {train_error}')
+        logger.info(f'For j (d-th dim of y)  = {j}: TT-ALS Relative error on test set = {val_error}')
         logger.info("======== Finished TT-ALS training ============")
-    return ETT
+    return ETTs
 
 
-def verify_fit_ETT(ETT_fit: Extended_TensorTrain,cnf_trajectory_model : CNF, target_distribution: torch.distributions.Distribution,
-                   base_distribution: torch.distributions.Distribution, t0: float, tN: float)->float:
+def verify_fit_ETT(ETTs_fit: List[Extended_TensorTrain], cnf_trajectory_model: CNF,
+                   target_distribution: torch.distributions.Distribution,
+                   base_distribution: torch.distributions.Distribution, t0: float, tN: float, n_samples: int,
+                   h: float, tN_minus_h: float) -> float:
     """
     In words, this function verify that  : Given a fitted ETT model (TT + Basis) and a fitted CNF trajectory model
     ( cnf_trajectory model) a sample x following a base distribution p_X can be
@@ -137,7 +136,81 @@ def verify_fit_ETT(ETT_fit: Extended_TensorTrain,cnf_trajectory_model : CNF, tar
         iv) calculate log_prob for the base_distribution give z(t0) , should be close to zero
     :return: log_prob_base_dist_z0  log_prob(base_distribution | z0)
     """
-    pass
+    # step i)
+
+    y = target_distribution.sample(torch.Size([n_samples]))  # [z_tN]
+    logp_diff_tN = torch.zeros(n_samples, 1).type(torch.float32).to(device)
+    # fixme : start sanity check (0)
+    y_np = y.detach().numpy()
+    normality_test_results_0 = pg.multivariate_normality(X=y_np)
+    # fixme : end sanity check (0)
+    # ---
+    # fixme : start sanity check (1)
+    zt, _ = odeint(func=trajectory_model, y0=(y, logp_diff_tN),
+                   t=torch.tensor([tN_minus_h, t0]))
+    zt0_hat = zt[-1]
+    zt0_hat_np = zt0_hat.detach().numpy()
+    normality_test_results_1 = pg.multivariate_normality(X=zt0_hat_np)
+    # fixme : end sanity check (1)
+
+    Dy = y.shape[1]
+    y_aug = torch.cat([y, torch.tensor([tN]).repeat(n_samples, 1)], dim=1)  # [z_tN,tN]
+    y_aug_domain_adjusted = adjust_tensor_to_domain(x=y_aug, domain_stripe=[-1, 1])  # fixme, make parametric
+    y_domain_adjusted_min = torch.min(y_aug_domain_adjusted, dim=0)
+    y_domain_adjusted_max = torch.max(y_aug_domain_adjusted, dim=0)
+    dzdt_list = []
+    for j in range(Dy):
+        ETTs_fit[j].tt.set_core(Dy - 1)
+        dzdt_j = ETTs_fit[j](y_aug_domain_adjusted)  # applied to [z_tN,tN]
+        dzdt_list.append(dzdt_j)
+    dzdt_tensor = torch.cat(dzdt_list, dim=1)
+    z_tN_domain_adjusted = y_aug_domain_adjusted[:, :Dy]
+    z_tN_domain_adjusted_min = torch.min(z_tN_domain_adjusted, dim=0)
+    z_tN_domain_adjusted_max = torch.max(z_tN_domain_adjusted, dim=0)
+    z_tN_minus_h = z_tN_domain_adjusted - h * dzdt_tensor
+    # fixme : start sanity check (2)
+    z_tN_minus_h_np = z_tN_minus_h.detach().numpy()
+    normality_test_results_2 = pg.multivariate_normality(X=z_tN_minus_h_np)
+    # fixme : end sanity check (2)
+    # step iii)
+    assert abs(h - (tN - float(tN_minus_h))) < 0.001
+
+    # z_tN_minus_h.type(torch.float32)
+    z_t_hybrid, _ = odeint(func=trajectory_model, y0=(z_tN_minus_h.type(torch.float32), logp_diff_tN),
+                           t=torch.tensor([tN_minus_h, t0]))
+    z_t0_hat_hybrid = z_t_hybrid[-1]
+    # fixme : start sanity check(3)
+    z_t0_hybrid_hat_np = z_t0_hat_hybrid.detach().numpy()
+    normality_test_results_3 = pg.multivariate_normality(X=z_t0_hybrid_hat_np)
+    # fixme : end sanity check(3)
+    # TODO : might be that domain adjustment distort the distribution\
+    # ---
+    # fixme : start sanity check (4)
+    z_t_domain_adjusted, _ = odeint(func=trajectory_model, y0=(z_tN_domain_adjusted.type(torch.float32), logp_diff_tN),
+                                    t=torch.tensor([tN, t0]))
+    z_tN_no_adjustment = y[:, :Dy]
+    z_t_no_adjustment, _ = odeint(func=trajectory_model, y0=(z_tN_no_adjustment.type(torch.float32), logp_diff_tN),
+                                  t=torch.tensor([tN, t0]))
+    normality_test_results_4_1 = pg.multivariate_normality(X=z_t_domain_adjusted[-1].detach().numpy())
+    normality_test_results_4_2 = pg.multivariate_normality(X=z_t_no_adjustment[-1].detach().numpy())
+    # fixme : end sanity check (4)
+    """
+    Status
+    It seems that domain adjustment distort the distribution when going through the CNF Trajectory, see 
+    normality_test 4_1 and 4_2
+    https://drive.google.com/file/d/1WV6HQdByzlxauRGUTEdqgZ2w-m-RMK0H/view?usp=drive_link
+    
+    # One possible solution : train Trajectory model on domain adjusted data ? 
+    steps : (for ordinary CNF) 
+    i) Generate Samples from target distribution
+    ii) Make domain adjustment ( -1 to 1) 
+    iii) Retest normality for domain adjustment
+    iv) Train CNF 
+    v) Train TT-ALS over h segment
+    vi) Re-test the hybrid trajectory generation
+    
+    """
+    logger.info(f'Verification Finished')
 
 
 if __name__ == '__main__':
@@ -152,8 +225,8 @@ if __name__ == '__main__':
 
     # Verify normality of generated data
     ## 1. Generate samples out of the loaded model and meta-data
-    z_tN = artifact['target_distribution'].sample(torch.Size([N_SAMPLES])).to(device)
-    logp_diff_tN = torch.zeros(N_SAMPLES, 1).type(torch.float32).to(device)
+    z_tN = artifact['target_distribution'].sample(torch.Size([args.n_samples])).to(device)
+    logp_diff_tN = torch.zeros(args.n_samples, 1).type(torch.float32).to(device)
     t0 = artifact['args']['t0']
     tN = artifact['args']['t1']
     t_vals = torch.tensor(list(np.arange(tN, t0 - 1, -1)))
@@ -189,16 +262,19 @@ if __name__ == '__main__':
 
     # 3. Run TT-ALS for the RK-step : z(tN) -> z(tN-h)
     t_N = t_vals[0]
-    t_N_minus_h = t_vals[args.h_steps]
-    h = t_N - t_N_minus_h
+    tN_minus_h = t_vals[args.h_steps]
+    h = t_N - tN_minus_h
     # mind the signs !!
     z_tN = z_t[0]
     z_tN_minus_h = z_t[args.h_steps]
     y = (z_tN_minus_h - z_tN) / (-h)
     x = z_tN
-
-    ETT_fit = run_tt_als(x=x, y=y, t=t_N, poly_degree=args.degree, rank=args.rank, test_ratio=0.2, tol=args.tol)
+    ETTs_fit = run_tt_als(x=x, y=y, t=t_N, poly_degree=args.degree, rank=args.rank, test_ratio=0.2, tol=args.tol)
     logger.info('Fitting TT-ALS for the sub-trajectory finished')
+    verify_fit_ETT(ETTs_fit=ETTs_fit, cnf_trajectory_model=trajectory_model,
+                   target_distribution=artifact['target_distribution'], base_distribution=artifact['base_distribution'],
+                   t0=artifact['args']['t0'], tN=artifact['args']['t1'], n_samples=args.n_samples, h=h,
+                   tN_minus_h=tN_minus_h)
     #
     # # double test with hybrid trajectory
     # ## 1. Explicit Euler step

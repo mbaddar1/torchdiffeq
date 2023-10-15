ec5f7c08d637e6f4e3525507b64ef43006a3784e
mbaddar <mbaddar2@gmail.com>
2023-10-15 13:48:18 +0200

    code to test the data-depth concept in practice

---
+++ OT/data_depth_testing.py
@@ -0,0 +1,12 @@
+"""
+
+Refs
+Multivariate Order Statistics:
+Theory and Application
+Grant B. Weller1 and William F. Eddy
+https://www.annualreviews.org/doi/pdf/10.1146/annurev-statistics-062614-042835
+
+From geometric quantiles to halfspace depths:
+A geometric approach for extremal behaviour.
+https://arxiv.org/pdf/2306.10789.pdf
+"""

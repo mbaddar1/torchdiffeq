6544064a381df9bb723acb4dcbc49562d5f80edb
mbaddar <mbaddar2@gmail.com>
2023-10-18 12:49:19 +0200

    started componen analysis testing

---
+++ OT/component_analysis_test.py
@@ -0,0 +1,18 @@
+"""
+Test PCA for composing and reconstructing Multivariate Distributions
+
+
+Comparing patterns of component loadings: Principal
+Component Analysis (PCA) versus Independent Component
+Analysis (ICA) in analyzing multivariate non-normal data
+https://link.springer.com/article/10.3758/s13428-012-0193-1#:~:text=For%20the%20multivariate%20normal%20distribution,ICA%20results%20will%20be%20similar.
+
+Scribe Notes: Multivariate Gaussians and PCA
+https://www.cs.princeton.edu/courses/archive/fall10/cos513/notes/2010-11-15.pdf
+
+PCA vs ICA
+https://www.quora.com/Are-there-implicit-Gaussian-assumptions-in-the-use-of-PCA-principal-components-analysis
+http://compneurosci.com/wiki/images/4/42/Intro_to_PCA_and_ICA.pdf
+"""
+if __name__ == '__main__':
+    pass

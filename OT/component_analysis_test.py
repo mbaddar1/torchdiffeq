08a630a56972de363b65d1181f2b483203fc06e0
mbaddar <mbaddar2@gmail.com>
2023-10-18 13:14:56 +0200

    adding ICA ref

--- OT/component_analysis_test.py
+++ OT/component_analysis_test.py
@@ -13,6 +13,10 @@ https://www.cs.princeton.edu/courses/archive/fall10/cos513/notes/2010-11-15.pdf
 PCA vs ICA
 https://www.quora.com/Are-there-implicit-Gaussian-assumptions-in-the-use-of-PCA-principal-components-analysis
 http://compneurosci.com/wiki/images/4/42/Intro_to_PCA_and_ICA.pdf
+
+
+Independent Component Analysis A Tutorial
+https://www.cs.jhu.edu/~ayuille/courses/Stat161-261-Spring14/HyvO00-icatut.pdf
 """
 if __name__ == '__main__':
     pass

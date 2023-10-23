915a70233234232cc7f0a33a3f029316f7f742fb
mbaddar <mbaddar2@gmail.com>
2023-10-23 16:40:38 +0200

    script to test inverse of CDF using quantiles and interp

---
+++ OT/inverse_transform_samples_quantile_interp.py
@@ -0,0 +1,10 @@
+"""
+https://stackoverflow.com/a/44163082
+
+https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.wasserstein_distance.html
+
+plan
+1. Implement a function that takes quantiles and generate samples
+2. Test Generated samples using WD distance
+3. Test on Normal, Exp, LogNormal and Empirical Dist from ICA transformation
+"""

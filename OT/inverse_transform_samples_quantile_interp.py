2262e8d7f0a81b019c9b84842b01ff3d84251dcc
mbaddar <mbaddar2@gmail.com>
2023-10-25 12:38:24 +0200

    TODO

--- OT/inverse_transform_samples_quantile_interp.py
+++ OT/inverse_transform_samples_quantile_interp.py
@@ -4,9 +4,11 @@ https://stackoverflow.com/a/44163082
 https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.wasserstein_distance.html
 
 plan
-1. Implement a function that takes quantiles and generate samples
+1. Implement a function that takes quantiles and generate samples Done
 2. Test Generated samples using WD distance
 3. Test on Normal, Exp, LogNormal and Empirical Dist from ICA transformation
+4- Use Cubic Spline
+https://docs.scipy.org/doc/scipy/reference/generated/scipy.interpolate.CubicSpline.html
 """
 import numpy as np
 import torch
